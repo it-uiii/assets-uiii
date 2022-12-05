42586d795d74a6d909eadd317eb385801ae2e25e
it-uiii <ict@uiii.ac.id>
2022-12-05 16:15:17 +0700

    fix

--- resources/views/logistics/create.blade.php
+++ resources/views/logistics/create.blade.php
@@ -31,9 +31,7 @@
                         <div>
                             <select class="form-control @error('satuan') is-invalid @enderror" name="satuan" required>
                                 <option value="">Pilih</option>
-                                <option value="Unit">Unit</option>
-                                <option value="Set">set</option>
-                                <option value="Pack">Pack</option>
+                                <option value="Pack">Box</option>
                                 <option value="Pcs">Pcs</option>
                             </select>
                             @error('satuan')
